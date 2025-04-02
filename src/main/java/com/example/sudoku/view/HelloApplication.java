c433db43df57b8533a6ae7ff8c18e73084ad0154
Valentina Nitola Alarcon <valentina.nitola@correounivalle.edu.co>
2025-04-01 21:53:12 -0500

    se agrega documentacion para JAVADOC

--- src/main/java/com/example/sudoku/view/HelloApplication.java
+++ src/main/java/com/example/sudoku/view/HelloApplication.java
@@ -1,18 +1,34 @@
 package com.example.sudoku.view;
 
-import com.example.sudoku.view.MenuView;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
+/**
+ * Clase principal que inicia la aplicación Sudoku.
+ * @author Valentina Nitola
+ * @version 1.0
+ */
 public class HelloApplication extends Application {
+
+    /**
+     * Método de inicio de la aplicación.
+     * Carga y muestra la vista del menú principal.
+     *
+     * @param stage el escenario principal de la aplicación.
+     * @throws IOException si ocurre un error al cargar la vista del menú.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         MenuView menuView = MenuView.getInstance();
         menuView.show();
-
     }
+
+    /**
+     * Método principal que lanza la aplicación.
+     *
+     * @param args argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
         launch();
     }
