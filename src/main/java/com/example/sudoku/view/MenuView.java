cb7d4c2ba1fa25384602996debfe2c03cd8ed49c
Valentina Nitola Alarcon <valentina.nitola@correounivalle.edu.co>
2025-04-01 21:48:42 -0500

    se agrega documentacion para JAVADOC

--- src/main/java/com/example/sudoku/view/MenuView.java
+++ src/main/java/com/example/sudoku/view/MenuView.java
@@ -6,10 +6,22 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
+/**
+ * Clase que representa la vista del menú principal del juego Sudoku.
+ *
+ * @author Valentina Nitola
+ * @version 1.0.3
+ */
 public class MenuView extends Stage {
-
+    /**
+     * Controlador asociado a la vista del menú.
+     */
     private MenuController controller;
-
+    /**
+     * Constructor que inicializa la vista del menú cargando el archivo FXML correspondiente.
+     *
+     * @throws IOException si ocurre un error al cargar el archivo FXML.
+     */
     public MenuView() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
                 HelloApplication.class.getResource("/com/example/sudoku/view/menuView.fxml")
@@ -19,21 +31,33 @@ public class MenuView extends Stage {
         this.setTitle("SUDOKU - MENU");
         this.setScene(scene);
     }
-
+    /**
+     * Obtiene el controlador de la vista del menú.
+     *
+     * @return el controlador de la vista del menú.
+     */
     public MenuController getController() {
         return controller;
     }
-
+    /**
+     *
+     * @return instancia única de {@link MenuView}.
+     * @throws IOException si ocurre un error al crear la instancia.
+     */
     public static MenuView getInstance() throws IOException {
         if (MenuViewHolder.INSTANCE == null) {
             MenuViewHolder.INSTANCE = new MenuView();
-            return MenuViewHolder.INSTANCE;
-        } else {
-            return MenuViewHolder.INSTANCE;
         }
+        return MenuViewHolder.INSTANCE;
     }
 
+    /**
+     * Clase interna estática que implementa el patrón Singleton para {@link MenuView}.
+     */
     private static class MenuViewHolder {
+        /**
+         * Instancia única de {@link MenuView}.
+         */
         private static MenuView INSTANCE;
     }
 }
